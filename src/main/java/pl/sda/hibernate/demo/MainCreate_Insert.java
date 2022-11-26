36897a3435ae8f9da5cd0768912c0eed47e71053
Paulina Pionk <paulinapionk@gmail.com>
2022-11-26 14:11:56 +0100

    SELECT FROM

---
+++ src/main/java/pl/sda/hibernate/demo/MainCreate_Insert.java
@@ -0,0 +1,56 @@
+package pl.sda.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.time.LocalDate;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project j133-hibernate-demo
+ * @created 26.11.2022
+ */
+public class MainCreate_Insert {
+    public static void main(String[] args) {
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+
+            // CREATE
+            // READ
+            // UPDATE
+            // DELETE
+
+            // Tworzymy studenta
+//            Student student = new Student();
+//            student.setImie("Paweł");
+//            student.setDataUrodzenia(LocalDate.of(1990, 1, 3));
+//            student.setKierunekNauczania("Informatyka");
+//            student.setIndeks("123123");
+
+//            Student student = new Student("Paweł", LocalDate.of(1990, 1, 3), "Informatyka", "123123");
+            Student student = Student.builder()
+                    .dataUrodzenia(LocalDate.of(1990, 1, 3))
+                    .kierunekNauczania("Informatyka")
+                    .indeks("123123")
+                    .imie("Paweł")
+                    .build();
+
+            // SQL: INSERT INTO 'student' values (...)
+            // zapisujemy studenta
+            // Merge:
+            //  - służy do aktualizacji,
+            //  - wymaga podania id, co jest kryterium aktualizacji (aktualizujemy obiekt o podanym id)
+            // Persist:
+            //  - służy do wstawiania NOWYCH rekordów do bazy,
+            //  - przeważnie nie podaje się ID ponieważ jest generowane
+            session.persist(student);
+
+            // zatwierdzamy transakcję
+            transaction.commit();
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
+    }
+}
---
+++ src/main/java/pl/sda/hibernate/demo/MainSelectByID.java
@@ -0,0 +1,21 @@
+package pl.sda.hibernate.demo;
+
+import jakarta.persistence.TypedQuery;
+import org.hibernate.Session;
+
+import org.hibernate.Session;
+
+public class MainSelectByID {
+    public static void main(String[] args) {
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Student student = session.get(Student.class, 2L);
+
+            System.out.println("Znaleźliśmy studenta: " + student);
+
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+            System.err.println("Błąd bazy: " + ioe);
+        }
+    }
+}
