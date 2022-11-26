a30274f203d7a0f4192a76993698635915c4e4c6
Paulina Pionk <paulinapionk@gmail.com>
2022-11-26 11:39:57 +0100

    Baza skonfigurowana

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,9 +1,15 @@
 package pl.sda.hibernate.demo;
 
+
+import org.hibernate.Session;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+        session.close();
     }
 
 }
