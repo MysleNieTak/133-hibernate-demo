11d6b6e672c2b040941c534b64c74c835861f53d
Paulina Pionk <paulinapionk@gmail.com>
2022-11-26 13:46:03 +0100

    Transakcje

--- src/main/java/pl/sda/hibernate/demo/Student.java
+++ src/main/java/pl/sda/hibernate/demo/Student.java
@@ -1,13 +1,19 @@
 package pl.sda.hibernate.demo;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
+
+
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+
 
 public class Student {
 
@@ -19,12 +25,14 @@ public class Student {
     // @NoArgsConstructor
 
 
-@Id // PRIMARY KEY
-@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT
-private Long id;                                                       // `id`                      INT PRIMARY KEY AUTO_INCREMENT,
+@Id                                                     // PRIMARY KEY
+@GeneratedValue(strategy = GenerationType.IDENTITY)     // AUTO INCREMENT
+    private Long id;                                                       // `id`                      INT PRIMARY KEY AUTO_INCREMENT,
     @Column(nullable = false)
     private String imie;                                               // `imie`                    VARCHAR(255),
     private LocalDate dataUrodzenia;                                   // `data_urodzenia`          DATE,
     private String kierunekNauczania;                                   // `kierunek_nauczania`     VARCHAR (255),
     private String indeks; //index jest słowem zabrionionym przez mysql // `indeks`                 VARCHAR(255),
-}
+
+    }
+
